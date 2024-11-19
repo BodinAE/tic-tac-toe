a7a248f88041b037ccbd9f5f8a69fd0259783d65
FluffyTowels <63123880+FluffyTowels@users.noreply.github.com>
2024-11-20 02:40:54 +0700

    Add project files.

---
+++ tic-tac-toe/Model/GameState.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tic_tac_toe
+{
+    internal class GameState : PropertyChangedClass
+    {
+        private char[] row1;
+        public char[] Row1
+        {
+            get
+            {
+                return row1;
+            }
+            set
+            {
+                row1 = value;
+                OnPropertyChanged("Row1");
+            }
+        }
+        
+        private char[] row2;
+        public char[] Row2
+        {
+            get
+            {
+                return row2;
+            }
+            set
+            {
+                row2 = value;
+                OnPropertyChanged("Row2");
+            }
+        }
+        
+        private char[] row3; 
+        public char[] Row3
+        {
+            get
+            {
+                return row3;
+            }
+            set
+            {
+                row3 = value;
+                OnPropertyChanged("Row3");
+            }
+        }
+
+        public int TurnNumber { get; set; }
+        public GameState()
+        {
+            Row1 = new char[] { ' ', ' ', ' ' };
+            Row2 = new char[] { ' ', ' ', ' ' };
+            Row3 = new char[] { ' ', ' ', ' ' };
+            TurnNumber = 1;
+        }
+    }
+}
---
+++ tic-tac-toe/ViewModel/TTTVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tic_tac_toe
+{
+    internal class TTTVM : PropertyChangedClass
+    {
+        public GameState GS { get; set; }
+
+        private RelayCommand placeComm;
+        public RelayCommand PlaceComm
+        {
+            get
+            {
+                return placeComm ?? (placeComm = new RelayCommand(obj =>
+                {
+                    char c = (char)obj;
+                    if (c == ' ')
+                    {
+                        if (GS.TurnNumber % 2 == 0)
+                        {
+                            c = 'X';
+                        }
+                        else
+                        {
+                            c = 'O';
+                        }
+                        obj = c;
+                        GS.TurnNumber++;
+                    }
+                }));
+            }
+        }
+        public TTTVM() 
+        {
+            GS = new GameState();
+        }
+    }
+}
